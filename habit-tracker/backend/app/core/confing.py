816860452553b5d15b39a8e52f5cb5c775be7b40
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-05-27 15:19:29 +0430

    feat(security): add refresh token secret key and refresh token expire minutes for Settings class

--- habit-tracker/backend/app/core/confing.py
+++ habit-tracker/backend/app/core/confing.py
@@ -5,9 +5,11 @@ class Settings(BaseSettings):
     MONGO_URL: str
     DATABASE_NAME: str
     
-    SECRET_KEY: str
+    ACCESS_SECRET_KEY: str
+    REFRESH_SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: str
 
     class Config:
         env_file = ".env"
