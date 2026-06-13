b1c51c62af014a4a483c6d5c445cabb062559fa7
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-13 20:35:51 +0430

    refactor(current-user): add checking user exists in get_current_user()

--- habit-tracker/backend/app/core/oauth2.py
+++ habit-tracker/backend/app/core/oauth2.py
@@ -1,62 +1,79 @@
-from fastapi.concurrency import run_in_threadpool
-from jose import jwt, JWTError
-from datetime import datetime, timezone, timedelta
+from datetime import (
+    datetime,
+    timezone,
+    timedelta
+)
 from typing import Dict
+from jose import jwt
+from jose import JWTError
+from fastapi import (
+    HTTPException,
+    status
+)
+from fastapi.concurrency import run_in_threadpool
 
 from app.schemas.token import TokenData
 from app.core.confing import settings
 
-
+# secret key
 ACCESS_SECRET_KEY = settings.ACCESS_SECRET_KEY
 REFRESH_SECRET_KEY = settings.REFRESH_SECRET_KEY
 
 ALGORITHM = settings.ALGORITHM
 
+# expire time
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 REFRESH_TOKEN_EXPIRE_DAYS = settings.REFRESH_TOKEN_EXPIRE_DAYS
 
 async def create_access_token(data: Dict):
+
     to_encode = data.copy()
 
+    # create expire minutes
     expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    
+
     to_encode.update({
         "exp": expire,
-        "type": "access"  
+        "type": "access"
     })
 
-    return  await run_in_threadpool(
+    # create JWT access-token
+    return await run_in_threadpool(
         jwt.encode,
         to_encode,
         ACCESS_SECRET_KEY,
-        algorithm = ALGORITHM
+        ALGORITHM
     )
 
-
-async def verify_access_token(token: str, credentials_exception: str):
+async def verify_access_token(token: str,credentials_exception):
+    
     try:
+        # verify JWT token
         payload = await run_in_threadpool(
             jwt.decode,
             token,
             ACCESS_SECRET_KEY,
-            algorithms = [ALGORITHM]
+            [ALGORITHM]
         )
 
         if payload.get("type") != "access":
             raise credentials_exception
-        
-        id: str = payload.get("user_id")
-        role: str = payload.get("role")
 
-        if id is None or role is None:
+        id = payload.get("id")
+        role = payload.get("role")
+
+        if not id or not role:
             raise credentials_exception
-        
-        return TokenData(id = str(id), role = role)
+
+        return TokenData(
+            id = id,
+            role = role
+        )
 
     except JWTError:
         raise credentials_exception
-    
-async def create_refresh_token(data: Dict): 
+
+async def create_refresh_token(data: Dict):
     to_encode = data.copy()
 
     expire = datetime.now(timezone.utc) + timedelta(days = REFRESH_TOKEN_EXPIRE_DAYS)
@@ -66,27 +83,36 @@ async def create_refresh_token(data: Dict):
         "type": "refresh"
     })
 
+    # create JWT refresh-token
     return await run_in_threadpool(
         jwt.encode,
         to_encode,
         REFRESH_SECRET_KEY,
-        algorithm = ALGORITHM
+        ALGORITHM
     )
 
-async def verify_refresh_token(token: str, credentials_exception: str):
+async def verify_refresh_token(token: str):
 
     try:
+        # vreify refresh-token
         payload = await run_in_threadpool(
             jwt.decode,
             token,
             REFRESH_SECRET_KEY,
-            algorithm = [ALGORITHM]
+            [ALGORITHM]
         )
 
         if payload.get("type") != "refresh":
-            raise credentials_exception
-        
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Invalid refresh token"
+            )
+
         return payload
-    
+
     except JWTError:
-        raise credentials_exception
+
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid refresh token"
+        )
--- habit-tracker/backend/app/dependencies/current_user.py
+++ habit-tracker/backend/app/dependencies/current_user.py
@@ -2,14 +2,32 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from app.core.oauth2 import verify_access_token
+from app.models.user import User
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl = 'login')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "login")
 
-async def get_current_user(token: str = Depends(oauth2_schema)):
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    # create credential exception
     credential_exception = HTTPException(
-        status_code = status.HTTP_401_UNAUTHORIZED, 
-        detail = "Could not validate credentials", 
-        headers = {"WWW-Authenticate": "bearer"}
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = "Could not validate credentials",
+        headers = {"WWW-Authenticate": "Bearer"}
     )
 
-    return await verify_access_token(token, credential_exception)
+    # verify JWT access-token 
+    token_data = await verify_access_token(token,credential_exception)
+
+    # get user from database
+    user = await User.get(token_data.id)
+
+    if not user:
+        raise credential_exception
+
+    # if user is deleted or inactive
+    if user.status != "active":
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "User account is inactive"
+        )
+
+    return user
