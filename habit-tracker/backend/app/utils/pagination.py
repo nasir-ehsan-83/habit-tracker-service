69dec80336383268815f287020e7c005627066c0
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-13 21:46:20 +0430

    feat: add validation and edge case handling to pagination function in utils/pagination.py

--- habit-tracker/backend/app/utils/pagination.py
+++ habit-tracker/backend/app/utils/pagination.py
@@ -1,5 +1,13 @@
-from typing import Optional, Tuple
+from typing import Tuple
 
-def paginate(page: Optional[int] = 1, limit: Optional[int] = 10) -> Tuple[int, int]:
+def paginate(page: int = 1, limit: int = 10) -> Tuple[int, int]:
+    # check that number of page should not be less than 1 or great than 10
+    page = min(max(1, page), 10)
+    
+    # check that number of linit should not be less than 1 or great than 10
+    limit = min(max(1, limit), 50)
+    
+    # calculate skip's value
     skip: int = (page - 1) * limit
+        
     return skip, limit
