6ffd4aee332da6d1ac675650468833d5cb958a7c
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-16 22:57:21 +0430

    add CORSMiddleware into main.py

--- habit-tracker/backend/app/main.py
+++ habit-tracker/backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi_offline_docs.offline_docs import setup_offline_docs
 
 from app.db.database import init_db
@@ -21,6 +23,19 @@ app = FastAPI(
 )
 
 setup_offline_docs(app)
+
+# add cores middleware
+app.add_middleware(
+    CORSMiddleware,
+    alowed_origins = [
+        "http://localhost:3000",    # for react dev
+        "http://127.0.0.1:5500",    # for liveserver
+        "https://www.google.com"    # for google
+    ],
+    alowed_credintials = True,
+    alowed_methods = ["GET, POST, DELETE, PATCH", "PUT"],
+    alowed_headers = ["*"]
+)
 
 # add routes from app/routes/
 app.include_router(auth.router)
