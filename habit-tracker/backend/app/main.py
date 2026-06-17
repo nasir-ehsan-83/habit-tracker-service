c4eed658ea16827472174909df5a796c694b983d
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-17 11:07:41 +0430

    feat: create rate-limiter in utils/limiter.py and add it in main.py

--- habit-tracker/backend/app/main.py
+++ habit-tracker/backend/app/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi import (
+    FastAPI,
+    Request,
+    status
+)
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from slowapi.errors import RateLimitExceeded
 
 from fastapi_offline_docs.offline_docs import setup_offline_docs
 
+from app.utils.limitter import limiter
 from app.db.database import init_db
 from app.routes import (
     auth, 
@@ -36,6 +43,19 @@ app.add_middleware(
     alowed_methods = ["GET, POST, DELETE, PATCH", "PUT"],
     alowed_headers = ["*"]
 )
+
+# add rate-limit
+app.state.limiter = limiter
+
+# add handler for rate-limit
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code = status.HTTP_429_TOO_MANY_REQUESTS,
+        content = {
+            "detail": "Too many requests. Please try again later."
+        }
+    )
 
 # add routes from app/routes/
 app.include_router(auth.router)
